69607f197c4e6c8e1144e103ad6835f20ed9403b
jackvischio <giacomo989jack@gmail.com>
2023-01-02 20:39:51 +0100

    deployment 1

--- database.py
+++ database.py
@@ -1,5 +1,4 @@
 from pymongo import MongoClient
 from os import environ as env
 
-#connection = MongoClient(settings.mongodb_uri)
-connection = env["MONGO_URI"]
+connection = MongoClient(env["MONGO_URI"])
--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from fastapi import FastAPI
-from routes.campionati import route
+import uvicorn
 
-app = FastAPI()
-
-app.include_router(route)
-
-@app.get("/")
-def index():
-    return {"message": "Welcome To FastAPI World"}
+if __name__ == "__main__":
+  uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
