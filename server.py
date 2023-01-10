037ebb17d4ef0c65a1b75ca0408e9645f3841fe3
jackvischio <giacomo989jack@gmail.com>
2023-01-10 22:24:43 +0100

    magari cosi funziona pure

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from routes.campionati import route as routeCamp
 from routes.arbitri import route as routeRef
+from routes.partite import route as routePart
 
 app = FastAPI()
 
 app.include_router(routeCamp)
 app.include_router(routeRef)
+app.include_router(routePart)
 
 @app.get("/")
 def index():
