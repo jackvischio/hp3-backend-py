84b7ec1b1d12b83f1d5eec673af11ec55d925fab
jackvischio <giacomo989jack@gmail.com>
2023-01-12 22:43:05 +0100

    CORS

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.campionati import route as routeCamp
 from routes.arbitri import route as routeRef
 from routes.partite import route as routePart
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(routeCamp)
 app.include_router(routeRef)
